acf9deca8c139e34b56e81c672e2c1b4960527b3
SURESH-S-U <suresh.elite07@gmail.com>
2025-02-12 11:31:03 +0530

    Small Scall Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>Real-time Face Detection</h1>
-      <img 
-        src="http://127.0.0.1:8000/video_feed" 
+      <img
+        src="http://127.0.0.1:8000/video_feed"
         alt="Face Detection Stream" 
         style={{ width: "640px", height: "480px", border: "2px solid black" }}
       />
